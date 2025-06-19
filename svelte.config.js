fe0007a57205a8dc9659588c5e0e44c097fa413a
Tilman M <mail@tlmn.io>
2025-06-19 08:46:53 +0200

    Add Content Security Policy (CSP) configuration to svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,26 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(), alias: {
+		adapter: adapter(), 
+		alias: {
 			"$assets": "src/assets",
 			"$src": "src",
+		},
+		csp: {
+			directives: {
+				'default-src': ['self'],
+				'script-src': ['self', 'unsafe-inline', 'unsafe-eval'],
+				'style-src': ['self', 'unsafe-inline'],
+				'img-src': ['self', 'data:', 'blob:'],
+				'font-src': ['self'],
+				'connect-src': ['self'],
+				'media-src': ['self', 'blob:'],
+				'object-src': ['none'],
+				'frame-src': ['none'],
+				'worker-src': ['self', 'blob:'],
+				'manifest-src': ['self']
+			},
+			mode: 'hash'
 		}
 	},
 };
