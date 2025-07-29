fec5fb71df02ccb3e37bac94b11a802cc563fdae
Tilman M <mail@tlmn.io>
2025-07-29 15:14:18 +0200

    add logo

--- src/lib/ui/Navigation.svelte
+++ src/lib/ui/Navigation.svelte
@@ -3,13 +3,17 @@
 	import { appState } from '$lib/stores/state.svelte';
 	import { stepsStore } from '$lib/stores/steps.svelte';
 	import { cn } from '$lib/utils/lib';
+	import Logo from '$assets/Logo.svelte';
 
 	let sufficientItems = $derived(appState.canvasItems.length < 2);
 </script>
 
-<nav>
-	<ul class="flex gap-2">
-		{#each STEPS as step, index}
+<nav class="flex w-full items-center">
+	<div>
+		<Logo />
+	</div>
+	<ul class="flex flex-1 justify-center gap-2">
+		{#each STEPS as step, index (step)}
 			<li>
 				<button
 					title={index === 2 && sufficientItems ? 'Please select some items to stitch first' : ''}
