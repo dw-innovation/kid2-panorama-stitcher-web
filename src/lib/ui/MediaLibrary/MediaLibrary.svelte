c436d20bc1dd62951a2a992099367c42bb524976
Tilman M <mail@tlmn.io>
2025-07-29 15:40:37 +0200

    fix order

--- src/lib/ui/MediaLibrary/MediaLibrary.svelte
+++ src/lib/ui/MediaLibrary/MediaLibrary.svelte
@@ -7,7 +7,6 @@
 	import groupBy from 'lodash/groupBy';
 	import flatMap from 'lodash/flatMap';
 	import sortBy from 'lodash/sortBy';
-	import { cn } from '$src/lib/utils/lib';
 
 	let orderedMediaItems = $derived(() => {
 		const items = [...appState.mediaItems];
@@ -29,24 +28,28 @@
 	});
 </script>
 
-<div class="mediaLibrary flex gap-2">
+<div class="mediaLibrary flex h-full flex-col">
 	<span class="pane-label">media library</span>
-	<div class="relative flex flex-nowrap gap-2 overflow-x-auto">
-		{#each orderedMediaItems() as mediaItem}
-			{#if mediaItem.mediaType === 'image'}
-				<Image {mediaItem} />
-			{/if}
 
-			{#if mediaItem.mediaType === 'video'}
-				<Video {mediaItem} />
-			{/if}
-		{/each}
-	</div>
-
-	<MediaDropZone>
-		<div class="flex min-w-24 flex-col items-center gap-2">
-			<PlusCircle />
-			<span class="text-center select-none">add media item</span>
+	<div class="flex min-h-0 flex-1 gap-2 p-2">
+		<div class="h-full w-32">
+			<MediaDropZone>
+				<div class="flex min-w-24 flex-col items-center gap-2">
+					<PlusCircle />
+					<span class="text-center select-none">add media item</span>
+				</div>
+			</MediaDropZone>
 		</div>
-	</MediaDropZone>
+		<div class="relative flex flex-nowrap gap-2 overflow-x-auto">
+			{#each orderedMediaItems() as mediaItem}
+				{#if mediaItem.mediaType === 'image'}
+					<Image {mediaItem} />
+				{/if}
+
+				{#if mediaItem.mediaType === 'video'}
+					<Video {mediaItem} />
+				{/if}
+			{/each}
+		</div>
+	</div>
 </div>
