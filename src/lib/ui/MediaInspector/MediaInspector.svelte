97ee0a273e5461022dd891ca6e8720540cc82bf0
Tilman M <mail@tlmn.io>
2025-08-05 14:10:17 +0200

    update finger

--- src/lib/ui/MediaInspector/MediaInspector.svelte
+++ src/lib/ui/MediaInspector/MediaInspector.svelte
@@ -18,7 +18,7 @@
 	<Container label="inspector" disabled>
 		<div class="flex h-full w-full items-center justify-center">
 			<div class="flex w-[10rem] flex-col items-center gap-2">
-				<enhanced:img src={Hand} class="w-[5rem] rotate-180" alt="hand" />
+				<enhanced:img src={Hand} class="w-[5rem]" alt="hand" />
 				<p class="text-center">select a media item from the gallery to inspect</p>
 			</div>
 		</div>
