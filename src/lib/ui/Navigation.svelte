0364ff6b408409bd837869c8bad0f57f1558f539
Tilman M <mail@tlmn.io>
2025-08-12 13:47:59 +0200

    add warning

--- src/lib/ui/Navigation.svelte
+++ src/lib/ui/Navigation.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import { appState } from '$lib/stores/state.svelte';
 	import Logo from '$assets/Logo.svelte';
 	import { modalState } from '../stores/modals.svelte';
 </script>
 
 <nav class="flex w-full items-center">
-	<div>
+	<div class="flex gap-4">
 		<Logo />
+		<span class="text-nowrap">This app works best in Chrome and Firefox </span>
 	</div>
 	<ul class="flex w-full items-center justify-end gap-4 text-sm text-gray-500!">
 		<li>
